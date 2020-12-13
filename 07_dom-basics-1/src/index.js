f441b421777ef6a5548f8e467bbaa2e1ba423741
iandypc <karpovandrey270896@yandex.ru>
2020-12-13 13:46:32 +0400

    First task done

--- 07_dom-basics-1/src/index.js
+++ 07_dom-basics-1/src/index.js
@@ -1,26 +1,28 @@
 'use strict';
 
 (function () {
-	let startCountButton = document.querySelector('.startCountButton')
-	let inputSeconds = document.querySelector('.inputSeconds')
-	let counterWindow = document.querySelector('.counterWindow')
+	let startCountButton = document.querySelector('.startCountButton');
+	let inputSeconds = document.querySelector('.inputSeconds');
+	let counterWindow = document.querySelector('.counterWindow');
 	let timerForCounter
 
 	startCountButton.addEventListener('click', () => {
-		clearInterval(timerForCounter)
-		let counterWindow_num = parseInt(inputSeconds.value)
+		clearInterval(timerForCounter);
+		let counterWindow_num = parseInt(inputSeconds.value);
 		if (isNaN(counterWindow_num)) {
-			alert('Введено не число')
-			clearInterval(timerForCounter)
+			alert('Введено не число');
+			clearInterval(timerForCounter);
 		}
-		counterWindow.textContent = counterWindow_num
+		counterWindow.textContent = counterWindow_num;
 
 		function counter () {
-			if (counterWindow_num > -1) {
-				counterWindow.textContent = counterWindow_num
+			if (counterWindow_num > 0) {
 				counterWindow_num--
+				counterWindow.textContent = counterWindow_num
+			} else {
+				clearInterval(timerForCounter);
 			}
 		}
-		timerForCounter = setInterval(counter, 1)
+		timerForCounter = setInterval(counter, 1000);
 	})
 })();
